0a49054603219154624532e964715419210687ca
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2021-11-18 16:11:44 +0200

    Return values via coroutine.yield and terminate objects with nil, Bayonet-style

--- init.lua
+++ init.lua
@@ -3,8 +3,10 @@ local print = function() end
 ----------------------------------------
 
 function parseJson(str, where)
+	coroutine.yield() -- wait for arguments
 	print("parseJson", where)
-	return parseElement(str, where)
+	where = parseElement(str, where)
+	return where
 end
 
 ----------------------------------------
@@ -20,9 +22,7 @@ function parseValue(str, where)
 	or     parseArray(str, where)
 	or     parseString(str, where)
 	or     parseNumber(str, where)
-	or     string.match(str, "^true()", where)
-	or     string.match(str, "^false()", where)
-	or     string.match(str, "^null()", where)
+	or     parseOther(str, where)
 end
 
 ----------------------------------------
@@ -30,23 +30,35 @@ end
 function parseObject(str, where)
 	print("parseObject", where)
 	if string.sub(str, where, where) ~= "{" then return end
+	coroutine.yield("object")
 	local there = string.match(str, "^[ \r\n\t]*}()", where + 1)
-	if there then return there end
-	where = parseMembers(str, where + 1)
-	if where == nil then return end
-	if string.sub(str, where, where) ~= "}" then return end
-	return where + 1
+	if there then
+		coroutine.yield()
+		return there
+	else
+		where = parseMembers(str, where + 1)
+		if where == nil then return end
+		if string.sub(str, where, where) ~= "}" then return end
+		coroutine.yield()
+		return where + 1
+	end
 end
 
 function parseArray(str, where)
 	print("parseArray", where)
 	if string.sub(str, where, where) ~= "[" then return end
+	coroutine.yield("array")
 	local there = string.match(str, "^[ \r\n\t]*%]()", where + 1)
-	if there then return there end
-	where = parseElements(str, where + 1)
-	if where == nil then return end
-	if string.sub(str, where, where) ~= "]" then return end
-	return where + 1
+	if there then
+		coroutine.yield()
+		return there
+	else
+		where = parseElements(str, where + 1)
+		if where == nil then return end
+		if string.sub(str, where, where) ~= "]" then return end
+		coroutine.yield()
+		return where + 1
+	end
 end
 
 function parseString(str, where)
@@ -56,17 +68,30 @@ function parseString(str, where)
 	while true do
 		before, after = string.match(str, '()\\*"()', after)
 		if before == nil then return end
-		if ((after - before) & 1) == 1 then return after end
+		if ((after - before) & 1) == 1 then
+			coroutine.yield("string", string.sub(str, where, after - 1))
+			return after
+		end
 	end
 end
 
 function parseNumber(str, where)
 	print("parseNumber", where)
+	local there = where
 	where = string.match(str, "^-()", where) or where
 	where = string.match(str, "^0()", where) or string.match(str, "^%d+()", where)
 	if where == nil then return end
 	where = string.match(str, "^%.%d+()", where) or where
-	return  string.match(str, "^[eE][-+]?%d+()", where) or where
+	where = string.match(str, "^[eE][-+]?%d+()", where) or where
+	coroutine.yield("number", string.sub(str, there, where - 1))
+	return where
+end
+
+function parseOther(str, where)
+	local there
+	there = string.match(str, "^true()", where); if there then coroutine.yield("true", true) return there end
+	there = string.match(str, "^false()", where); if there then coroutine.yield("false", false) return there end
+	there = string.match(str, "^null()", where); if there then coroutine.yield("null", nil) return there end
 end
 
 ----------------------------------------
@@ -116,4 +141,11 @@ end
 
 ----------------------------------------
 
-print(parseJson('"foo\\"bar" "ww"', 1))
+local print = _G.print
+print()
+local parseJson = coroutine.wrap(parseJson)
+-- parseJson('[1, 2, {"foo": 2, "bar": [3,  2, 1]}]', 1)
+parseJson(editor:GetText(), 1)
+for i = 1,100 do
+	print(parseJson())
+end
